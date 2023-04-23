aca8509d93f1abf87beaf480a01362d3b937dbfd
china_boy <70200814+wa1ki0g@users.noreply.github.com>
2023-04-23 23:04:03 +0800

    Create logo.go

---
+++ lib/logo.go
@@ -0,0 +1,14 @@
+package lib
+
+import "fmt"
+
+func Logo() {
+	fmt.Println(Yellow("\n------KFC-CRAZY-THURSDAY-VME50 !------"))
+	fmt.Println(Blue(`___ _ ___ ___ _ _____ _ ____
+\ \/// ___\/ ___\/ \ /|/ __// \ / __\
+ \ / | \| \| |_||| \ | | | \/|
+ / \ \___ |\___ || | ||| /_ | |_/\| __/
+/__/\\\____/\____/\_/ \|\____\\____/\_/
+`))
+	fmt.Println(Yellow("------KFC-CRAZY-THURSDAY-VME50 !------\n"))
+}
